c88602d83d7bb6690765285cc9503b7ee0ef25e1
Syed Taqi Rizvi <SP20MCS0005@MAJU.EDU.PK>
2023-03-01 12:31:36 +0500

    Controllers Using Scafolding Added

---
+++ RacersCommunity/Controllers/ClubsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RacersCommunity.Data;
+using RacersCommunity.Models;
+
+namespace RacersCommunity.Controllers
+{
+    public class ClubsController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ClubsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Clubs
+        public async Task<IActionResult> Index()
+        {
+            var applicationDBContext = _context.Club.Include(c => c.Address).Include(c => c.AppUser);
+            return View(await applicationDBContext.ToListAsync());
+        }
+
+        // GET: Clubs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Club == null)
+            {
+                return NotFound();
+            }
+
+            var club = await _context.Club
+                .Include(c => c.Address)
+                .Include(c => c.AppUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            return View(club);
+        }
+
+        // GET: Clubs/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressId"] = new SelectList(_context.Address, "Id", "Id");
+            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "Id");
+            return View();
+        }
+
+        // POST: Clubs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Image,AddressId,ClubCategory,AppUserId")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(club);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Address, "Id", "Id", club.AddressId);
+            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "Id", club.AppUserId);
+            return View(club);
+        }
+
+        // GET: Clubs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Club == null)
+            {
+                return NotFound();
+            }
+
+            var club = await _context.Club.FindAsync(id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressId"] = new SelectList(_context.Address, "Id", "Id", club.AddressId);
+            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "Id", club.AppUserId);
+            return View(club);
+        }
+
+        // POST: Clubs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Image,AddressId,ClubCategory,AppUserId")] Club club)
+        {
+            if (id != club.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(club);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClubExists(club.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Address, "Id", "Id", club.AddressId);
+            ViewData["AppUserId"] = new SelectList(_context.AppUser, "Id", "Id", club.AppUserId);
+            return View(club);
+        }
+
+        // GET: Clubs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Club == null)
+            {
+                return NotFound();
+            }
+
+            var club = await _context.Club
+                .Include(c => c.Address)
+                .Include(c => c.AppUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            return View(club);
+        }
+
+        // POST: Clubs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Club == null)
+            {
+                return Problem("Entity set 'ApplicationDBContext.Club'  is null.");
+            }
+            var club = await _context.Club.FindAsync(id);
+            if (club != null)
+            {
+                _context.Club.Remove(club);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClubExists(int id)
+        {
+          return _context.Club.Any(e => e.Id == id);
+        }
+    }
+}
